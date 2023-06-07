eb7347fe588adecc3e5d13f7d9a30bbff91b524e
catalfacu <cataldof21@gmail.com>
2023-06-07 13:52:43 -0300

    Haciendo el testing de RyM

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,43 +1,11 @@
-const express = require("express");
+
 const server = express();
-const router = require('./routes/index');
-const morgan = require('morgan');
 const PORT = 3001;
 
 
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-      );
-   res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-      );
-      next();
-});
-   
-   server.use(express.json());
-   server.use(morgan('dev'));
-   server.use('/rickandmorty' , router);
+
 
 server.listen(PORT, () => {
    console.log(('Server raised in port:' + PORT));
 });
 
-
-// const http = require('http');
-// const getCharacterById = require('./controllers/getCharacterById')
-
-
-// http.createServer((req,res)=>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//  if(req.url.includes("/rickandmorty/character")) {
-//     const id = req.url.split("/").pop();
-//     getCharacterById(res,id);
-//  }
- 
-// }).listen(3001,'localhost')
