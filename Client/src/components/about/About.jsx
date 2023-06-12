2167512a4ed6d0d3c594df26521bf20d806a6baa
catalfacu <cataldof21@gmail.com>
2023-06-12 00:41:50 -0300

    Ajustando css del componente about

--- Client/src/components/about/About.jsx
+++ Client/src/components/about/About.jsx
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from './About.module.css'
 
 
 export default function About(props) {
+
+  const { aboutView, setAboutView} = useState(false);
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      setAboutView(true);
+    },0o500)
+  },[]);
+
   return (
     <div className={styles.aboutContainer}>
         <div className={styles.text}>
@@ -10,7 +19,9 @@ export default function About(props) {
             <h1>FACUNDO CATALDO</h1> 
             <h3>About me:</h3>
         </div>
-      <div className={styles.imagen}><img src="\src\assets\Logos para el about\Diseñosintítulo.png" alt="mifoto" /></div>  
+      <div className={styles.imagen}><img src="\src\assets\Logos para el about\Diseñosintítulo.png" alt="mifoto" /></div>
+
+
     <div className={styles.linksStyle}>
        
       <a href="https://www.instagram.com/facucata23/?hl=es-la" target="_blank">
@@ -19,6 +30,7 @@ export default function About(props) {
           alt="instagram"
         />
       </a>
+      
       <a
         href="https://www.linkedin.com/in/facundo-cataldo-9a0031272/"
         target="_blank"
@@ -36,12 +48,14 @@ export default function About(props) {
         />
 
       </a>
+
       <a href="https://www.facebook.com/facu.cataldo.9" target="_blank">
       <img
           src="src\assets\Logos para el about\facebook-2.png"
           alt="facebook"
         />
       </a>
+
     </div>
       
     </div>
